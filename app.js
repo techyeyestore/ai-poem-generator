3662d7d4a7b0b7afea7d943c5d1869a97fc8e572
techyeyestore <129699711+techyeyestore@users.noreply.github.com>
2023-05-20 03:16:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 3000;
 
 const openAi = new OpenAIApi(
   new Configuration({
-    apiKey: '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',
+    apiKey: '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',
   })
 )
 
